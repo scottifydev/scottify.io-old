0280fca0dea2e2d90d68999c6804dda0449a1200
Scott Lee <scott@vertualize.com>
2018-04-29 17:26:52 -0400

    Added focus affects to Nav.css and linted Top.js

--- src/components/Intro/Page/Top/Top.js
+++ src/components/Intro/Page/Top/Top.js
@@ -1,23 +1,20 @@
-import { connect } from "react-redux";
-import React from "react";
+import { connect } from 'react-redux';
+import React from 'react';
 import cn from 'classnames';
 
-import me from '../../../../assets/me.png'
-import style from "./Top.css";
+import me from '../../../../assets/me.png';
+import style from './Top.css';
 
-const top = () => {
-    return (
-            <div className={cn(style.Round)} >
-                <img src={me} className={cn(style.Me)} />
-            </div>
-    )
-}
+const top = () => (
+    <div className={ cn( style.Round ) } >
+        <img src={ me } className={ cn( style.Me ) } alt="me" />
+    </div>
+);
 
-const mapStateToProps = state => {
-    return {
-        page: state.page
-    };
-}
+const mapStateToProps = state => (
+    {
+        page: state.page,
+    }
+);
 
-
-export default connect(mapStateToProps)(top);
+export default connect( mapStateToProps )( top );
