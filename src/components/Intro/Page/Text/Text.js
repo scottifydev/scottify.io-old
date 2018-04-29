6c6cc6af6bb9c43bbfb0256154ef5359a531108d
Scott Lee <scott@vertualize.com>
2018-04-29 16:05:37 -0400

    linted text.js

--- src/components/Intro/Page/Text/Text.js
+++ src/components/Intro/Page/Text/Text.js
@@ -1,34 +1,46 @@
-import React, { Component, Fragment } from "react";
-import { connect } from "react-redux";
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import cn from 'classnames';
 
-import style from "./Text.css";
+import style from './Text.css';
 
 class Text extends Component {
     content = [
-        "Hi, I'm Scott.", 
-        "This is some more Text", 
-        "And even more"
+        'Hi, I\'m Scott.',
+        'This is some more Text',
+        'And even more',
     ]
-    
-    handleText = () => <span className={cn(style.Caption, 'text-center')} ref={c => this.container = c}>{this.content[this.props.page - 1]}</span>;
 
-    render () {
+    handleText = () => (
+        <span
+            className={ cn( style.Caption, 'text-center' ) }
+        >
+            { this.content[ this.props.page - 1 ] }
+        </span>
+    );
+
+    render() {
         return (
             <Fragment>
                 {this.handleText()}
             </Fragment>
-        )
-
+        );
     }
-    
-}
-const mapStateToProps = state => {
-    return {
-        page: state.page
-    };
 }
 
+const mapStateToProps = state => (
+    {
+        page: state.page,
+    }
+);
 
-export default connect(mapStateToProps)(Text);
+export default connect( mapStateToProps )( Text );
 
+Text.propTypes = {
+    page: PropTypes.number,
+};
+
+Text.defaultProps = {
+    page: 1,
+};
